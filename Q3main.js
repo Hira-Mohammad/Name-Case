fce643100c8f92c2c482416e963266a0aff7111e
Hira Mohammad <161503313+Hira-Mohammad@users.noreply.github.com>
2024-03-10 15:53:31 +0500

    Add files via upload

---
+++ Q3main.js
@@ -0,0 +1,7 @@
+var firstname = "Hiramohammad";
+// shows the name in all small letters
+console.log(firstname.toLowerCase());
+// shows the name in ALL CAPITAL LETTERS 
+console.log(firstname.toUpperCase());
+// shows the name with First Letter Capital
+console.log(firstname.charAt(0).toUpperCase() + firstname.slice(1).toLowerCase());
---
+++ Q3main.ts
@@ -0,0 +1,8 @@
+
+let firstname: string = "Hiramohammad"
+// shows the name in all small letters
+console.log(firstname.toLowerCase());
+// shows the name in ALL CAPITAL LETTERS 
+console.log(firstname.toUpperCase());
+// shows the name with First Letter Capital
+console.log(firstname.charAt(0).toUpperCase() + firstname.slice(1).toLowerCase());
